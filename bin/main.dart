8c559abbcb9e4c30e74ca2c3d003f7b0bfc6dc9f
Bohne13 <j.bohne.13@gmail.com>
2018-02-14 18:41:03 +0100

    add: .idea/

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,75 @@
-import 'package:ladenverwaltung/ladenverwaltung.dart' as ladenverwaltung;
+library finance;
 
-main(List<String> arguments) {
-  print('Hello world: ${ladenverwaltung.calculate()}!');
+import 'dart:io';
+
+part 'customer.dart';
+part 'item.dart';
+
+List<Customer> customers = [];
+List<Item> items = [];
+
+String start() {
+  print("Wählen Sie eine Option:");
+  print(
+      "(NC) New Customer, (Qu)ite, (PC) print customer, (NI) new item,\n(PI) print item, (DC) delete customer, (DI) delete item, (DA) delete all,\n");
+  return stdin.readLineSync().toUpperCase();
+}
+
+void deleteAll() {
+  String temp = "";
+  print("Sind Sie sicher, alle KUNDEN und alle ARTIKEL zu löschen? (Y/N)");
+  do {
+    temp = stdin.readLineSync().toUpperCase();
+  } while (temp != "Y" && temp != "N");
+  if (temp == "Y") {
+    print("Sind Sie sich wirkling sicher? (Y/N)");
+    do {
+      temp = stdin.readLineSync().toUpperCase();
+    } while (temp != "Y" && temp != "N");
+    if (temp == "Y") {
+      customers.clear();
+      items.clear();
+      print("Alle Kunden und Artikel wurden gelöscht!");
+    } else {
+      return;
+    }
+  } else {
+    return;
+  }
+}
+
+void main() {
+  // programm loop
+  while (true) {
+    String option = start();
+    // exit if "Q" is selected
+    switch (option) {
+      case "QU":
+        exit(0);
+        break;
+      case "NC":
+        Customer.newCustomer();
+        break;
+      case "PC":
+        Customer.printCustomers();
+        break;
+      case "NI":
+        Item.newItem();
+        break;
+      case "PI":
+        Item.printItem();
+        break;
+      case "DC":
+        Customer.deleteCustomer();
+        break;
+      case "DA":
+        deleteAll();
+        break;
+      case "DI":
+        Item.deleteItem();
+        break;
+      default:
+        break;
+    }
+  }
 }
